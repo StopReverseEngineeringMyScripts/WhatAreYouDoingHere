6194f5e6f433666566c31167c6cd3d2c8a70386b
StopReverseEngineeringMyScripts <78369650+StopReverseEngineeringMyScripts@users.noreply.github.com>
2021-02-24 11:07:00 +0000

    Create AreUSmart.lua

---
+++ AreUSmart.lua
@@ -0,0 +1,138 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+local TextLabel_3 = Instance.new("TextLabel")
+local TextLabel_4 = Instance.new("TextLabel")
+local TextLabel_5 = Instance.new("TextLabel")
+local TextLabel_6 = Instance.new("TextLabel")
+local TextLabel_7 = Instance.new("TextLabel")
+local TextLabel_8 = Instance.new("TextLabel")
+local TextLabel_9 = Instance.new("TextLabel")
+local TextLabel_10 = Instance.new("TextLabel")
+local TextLabel_11 = Instance.new("TextLabel")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.Size = UDim2.new(0, 1761, 0, 1034)
+TextLabel.Position = UDim2.new(0.4, 0, 0.4, 0)
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.388983518, 0, 0.29883945, 0)
+TextLabel.Size = UDim2.new(0, 390, 0, 59)
+TextLabel.Font = Enum.Font.RobotoCondensed
+TextLabel.Text = "Retard Definition: What is a Retard?"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 30.000
+
+TextLabel_2.Parent = Frame
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.Position = UDim2.new(0.331629753, 0, 0.376208901, 0)
+TextLabel_2.Size = UDim2.new(0, 593, 0, 39)
+TextLabel_2.Font = Enum.Font.RobotoCondensed
+TextLabel_2.Text = "(informal, offensive) A person or being who is extremely stupid or slow to learn. "
+TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.TextSize = 30.000
+
+TextLabel_3.Parent = Frame
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BorderSizePixel = 0
+TextLabel_3.Position = UDim2.new(0.3310619, 0, 0.433268845, 0)
+TextLabel_3.Size = UDim2.new(0, 593, 0, 39)
+TextLabel_3.Font = Enum.Font.RobotoCondensed
+TextLabel_3.Text = "Quotations:"
+TextLabel_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.TextSize = 30.000
+
+TextLabel_4.Parent = Frame
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.BorderSizePixel = 0
+TextLabel_4.Position = UDim2.new(0.3310619, 0, 0.476789117, 0)
+TextLabel_4.Size = UDim2.new(0, 138, 0, 39)
+TextLabel_4.Font = Enum.Font.RobotoCondensed
+TextLabel_4.Text = "1. e621 is a"
+TextLabel_4.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_4.TextSize = 30.000
+
+TextLabel_5.Parent = Frame
+TextLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.BorderSizePixel = 0
+TextLabel_5.Position = UDim2.new(0.408858657, 0, 0.476789117, 0)
+TextLabel_5.Size = UDim2.new(0, 79, 0, 39)
+TextLabel_5.Font = Enum.Font.SourceSansBold
+TextLabel_5.Text = "Retard"
+TextLabel_5.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_5.TextSize = 30.000
+
+TextLabel_6.Parent = Frame
+TextLabel_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_6.BorderSizePixel = 0
+TextLabel_6.Position = UDim2.new(0.458262354, 0, 0.476789117, 0)
+TextLabel_6.Size = UDim2.new(0, 337, 0, 39)
+TextLabel_6.Font = Enum.Font.RobotoCondensed
+TextLabel_6.Text = "because He/She leaked GrubHub."
+TextLabel_6.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_6.TextSize = 30.000
+
+TextLabel_7.Parent = Frame
+TextLabel_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_7.BorderSizePixel = 0
+TextLabel_7.Position = UDim2.new(0.57069844, 0, 0.514506698, 0)
+TextLabel_7.Size = UDim2.new(0, 171, 0, 39)
+TextLabel_7.Font = Enum.Font.RobotoCondensed
+TextLabel_7.Text = "because they tried to use GrubHub"
+TextLabel_7.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_7.TextSize = 30.000
+
+TextLabel_8.Parent = Frame
+TextLabel_8.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_8.BorderSizePixel = 0
+TextLabel_8.Position = UDim2.new(0.461669534, 0, 0.514506698, 0)
+TextLabel_8.Size = UDim2.new(0, 79, 0, 39)
+TextLabel_8.Font = Enum.Font.SourceSansBold
+TextLabel_8.Text = "Retarded"
+TextLabel_8.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_8.TextSize = 30.000
+
+TextLabel_9.Parent = Frame
+TextLabel_9.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_9.BorderSizePixel = 0
+TextLabel_9.Position = UDim2.new(0.328222603, 0, 0.514506698, 0)
+TextLabel_9.Size = UDim2.new(0, 138, 0, 39)
+TextLabel_9.Font = Enum.Font.RobotoCondensed
+TextLabel_9.Text = "2. The person reading ths is"
+TextLabel_9.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_9.TextSize = 30.000
+
+TextLabel_10.Parent = Frame
+TextLabel_10.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_10.BorderSizePixel = 0
+TextLabel_10.Position = UDim2.new(0.467915922, 0, 0.546421587, 0)
+TextLabel_10.Size = UDim2.new(0, 171, 0, 39)
+TextLabel_10.Font = Enum.Font.RobotoCondensed
+TextLabel_10.Text = "without joining the discord server - which supports the creator."
+TextLabel_10.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_10.TextSize = 30.000
+
+TextLabel_11.Parent = Frame
+TextLabel_11.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_11.BorderSizePixel = 0
+TextLabel_11.Position = UDim2.new(0.491198182, 0, 0.605415821, 0)
+TextLabel_11.Size = UDim2.new(0, 88, 0, 13)
+TextLabel_11.Font = Enum.Font.RobotoCondensed
+TextLabel_11.Text = "- WickyPeedia 2021"
+TextLabel_11.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_11.TextSize = 20.000
